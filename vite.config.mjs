05588cbcd0649315bb29433cdac2f36d7a1eb60e
Emilyn <emilynbabu@gmail.com>
2026-03-24 19:38:01 +0100

    feat: initial commit

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,10 +1,18 @@
+import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   plugins: [
     react(),
+    tailwindcss(),
     VitePWA({
       registerType: 'autoUpdate',
       includeAssets: ['favicon.svg'],
